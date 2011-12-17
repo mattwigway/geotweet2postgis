f2cd3c506f9b8b7f67888634a7189564ae429f64
Matt Conway <mattwigway@gmail.com>
2011-12-17 10:13:44 -0800

    Added Apache 2.0

--- geotweet2postgis.py
+++ geotweet2postgis.py
@@ -1,3 +1,29 @@
+#!/usr/bin/python
+
+# geotweet2postgis.py: load geotagged tweets to a PostGIS database
+# Copyright 2011 Matt Conway
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Authors:
+# Matt Conway: main code
+
+# The table you connect to should look like this:
+# screename VARCHAR(25)
+# tweet VARCHAR(140)
+# the_geom GEOMETRY
+# I also recommend a SERIAL PRIMARY KEY
+
 from tweepy.streaming import StreamListener, Stream
 from tweepy.auth import BasicAuthHandler
 from psycopg2 import connect
